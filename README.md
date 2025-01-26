ca9f63c182d43eb188bf7403c7c6a33aad7e1123
HUANG SHICHAO <hsc28yo@gmail.com>
2025-01-27 01:43:48 +0800

    feat: middleware done

--- README.md
+++ README.md
@@ -4,3 +4,13 @@
 
 > Don't complain it, change it.  
 > 不要抱怨它，去改变它。
+
+## git commit keyword
+
+- feat: 新功能(feature)
+- fix: 修补bug
+- docs: 文档(documentation)
+- style: 格式(不影响代码运行的变动)
+- refactor: 重构(既不是新增功能,也不是修改bug的代码变动)
+- test: 增加测试
+- chore: 构建过程或辅助工具的变动
---
+++ elpis-core/env.js
@@ -0,0 +1,18 @@
+module.exports = {
+  // 判断是否本地环境
+  isLocal() {
+    return process.env.NODE_ENV === "local";
+  },
+  // 判断是否测试环境
+  isBeta() {
+    return process.env.NODE_ENV === "beta";
+  },
+  // 判断是否生产环境
+  isProduction() {
+    return process.env.NODE_ENV === "production";
+  },
+  // 获取当前环境
+  get() {
+    return process.env.NODE_ENV ?? "local";
+  },
+};
